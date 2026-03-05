9925d3e2d95b9f3b67dd3d136cea70f82467b87a
Andrej Lotski <andrej.lotski@sporty.com>
2026-03-05 10:03:55 +0100

    Initial Commit

---
+++ MeetApp/MeetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetAppApp.swift
+//  MeetApp
+//
+//  Created by Andrej Lotski on 5.3.26.
+//
+
+import SwiftUI
+
+@main
+struct MeetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
